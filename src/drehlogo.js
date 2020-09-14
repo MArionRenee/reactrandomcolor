bc4ef56a8148eb3b75140c4d6bdb6f603360d932
MArionRenee <marion.dortschack@gmx.net>
2020-09-14 23:48:04 +0200

    first commit

---
+++ src/drehlogo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App01() {
+  return (
+    <div className="App01">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to MArions ColorGenerator!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App01;
