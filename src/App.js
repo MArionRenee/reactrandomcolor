2de9d94593ba3ef10f7e6be183e6030b8cc64500
MArionRenee <marion.dortschack@gmx.net>
2020-09-22 17:22:24 +0200

    Rename variables in App.js

--- src/App.js
+++ src/App.js
@@ -69,11 +69,11 @@ const numbersBlue = [
 
 // Here I created the random color without any specifications.
 const App = () => {
-  const [hex, setHex] = useState('#ffffff');
-  const randomizedHex = () => {
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff');
+  const RandomBackgroundColor = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-    setHex(randomColor);
+    setBackgroundColor(randomColor);
   };
 
   //Here I tried to randomize the given colors.
@@ -81,19 +81,19 @@ const App = () => {
   const randomGreen = Math.floor(Math.random() * numbersGreen.length);
   const randomBlue = Math.floor(Math.random() * numbersBlue.length);
 
-  const shuffeledRed = () => {
+  const setRandomRedBackgroundColor = () => {
     const selectedRed = numbersRed[randomRed];
-    setHex(selectedRed);
+    setBackgroundColor(selectedRed);
   };
 
-  const shuffeledGreen = () => {
+  const setRandomGreenBackgroundColor = () => {
     const selectedGreen = numbersGreen[randomGreen];
-    setHex(selectedGreen);
+    setBackgroundColor(selectedGreen);
   };
 
-  const shuffeledBlue = () => {
+  const setRandomBlueBackgroundColor = () => {
     const selectedBlue = numbersBlue[randomBlue];
-    setHex(selectedBlue);
+    setBackgroundColor(selectedBlue);
   };
 
   //Here is the output for the background of all "random-colors".
@@ -101,7 +101,7 @@ const App = () => {
     <div
       className="App"
       style={{
-        backgroundColor: `${hex}`,
+        backgroundColor: `${backgroundColor}`,
         minHeight: '33.34vh',
         overflow: 'hidden',
         zoom: 1.5,
@@ -109,63 +109,34 @@ const App = () => {
 
       // Here I created the buttons.
     >
-      <h1>{hex}</h1>
-      <button onClick={randomizedHex}>Randomize</button>
-      <button onClick={() => navigator.clipboard.writeText(hex)}>
+      <h1>{backgroundColor}</h1>
+      <button onClick={RandomBackgroundColor}>Randomize</button>
+      <button onClick={() => navigator.clipboard.writeText(backgroundColor)}>
         Copy Color
       </button>
       <br></br>
-      <button style={{ backgroundColor: 'red' }} onClick={shuffeledRed}>
+      <button
+        style={{ backgroundColor: 'red' }}
+        onClick={setRandomRedBackgroundColor}
+      >
         RandomRed
       </button>
 
-      <button style={{ backgroundColor: 'green' }} onClick={shuffeledGreen}>
+      <button
+        style={{ backgroundColor: 'green' }}
+        onClick={setRandomGreenBackgroundColor}
+      >
         RandomGreen
       </button>
 
-      <button style={{ backgroundColor: 'blue' }} onClick={shuffeledBlue}>
+      <button
+        style={{ backgroundColor: 'blue' }}
+        onClick={setRandomBlueBackgroundColor}
+      >
         RandomBlue
       </button>
     </div>
   );
 };
 
-// This was a attempt to create a luminosity button:
-
-//return lighter (+lum) or darker (-lum) color as a hex string
-// pass original hex string and luminosity factor, e.g. -0.1 = 10% darker
-
-// function colorLuminance(hex, lum) {
-//   // validate hex string
-//   hex = String(hex).replace(/[^0-9a-f]/gi, '');
-//   if (hex.length < 6) {
-//     hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-//   }
-//   lum = lum || 0;
-
-//   // convert to decimal and change luminosity
-// const rgb = '#',
-//     d,
-//     b;
-//   for (b = 0; b < 3; b++) {
-//     d = parseInt(hex.substr(b * 2, 2), 16);
-//     d = Math.round(Math.min(Math.max(0, d + d * lum), 255)).toString(16);
-//     rgb += ('00' + d).substr(d.length);
-//   }
-
-//   return rgb;
-//}
-
-// const colorLum = 'randomColor',
-//   lum = 0.05,
-//   out = document.getElementById('out');
-
-// const b, d, nc;
-
-// for (const b = 0; b < 100; b++) {
-//   const d = out.appendChild(document.createElement('div'));
-//   nc = colorLuminance(color, b * lum);
-//   d.style.backgroundColor = nc;
-//   d.title = nc;
-// }
 export default App;
